91e745c07fdfb7f68dde4baa4493cce41a6ec7cc
lishnalei <2686685661@qq.com>
2018-12-26 13:25:00 +0800

    修改路径

--- app/Http/Controllers/Front/Colleges/MajorController.php
+++ app/Http/Controllers/Front/Colleges/MajorController.php
@@ -235,6 +235,8 @@
                 $get_activitys['info'][$key]->start_state = $now_time < $item->begin_time ? 0 : $now_time > $item->end_time ? 2 : 1;
                 $get_activitys['info'][$key]->begin_time = date("m-d",$item->begin_time);
                 $get_activitys['info'][$key]->end_time = date("m-d", $item->end_time);
+                $get_activitys['info'][$key]->active_img = splicingImgStr('admin', 'info', $item->active_img);
+                $get_activitys['info'][$key]->magor_logo_name = splicingImgStr('admin', 'info', $item->magor_logo_name);
                 if($item->province !== '')
                     $get_activitys['info'][$key]->province = getProCity($item->province);
             }
@@ -266,7 +268,10 @@
                 $inf_ids[$i] = $inf[$i]->zx_id;
     
             $fileds = ['id','zx_name','z_image','z_alt','brief_introduction','z_from','update_time'];
-            $information = zslm_information::getInfoByIds($inf_ids,$fileds);
+            $information = zslm_information::getInfoByIds($inf_ids,$fileds)->toArray();
+            foreach($information as $key => $item) {
+                $information[$key]->z_image = splicingImgStr('admin', 'info', $item->z_image);
+            }
             return responseToJson(0,'success',$information);
     
         }
