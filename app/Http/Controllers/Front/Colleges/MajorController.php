a00d1ab8dbf7860b7805f8254ff97de911bd3fef
lishnalei <2686685661@qq.com>
2019-02-26 20:14:16 +0800

    test

--- app/Http/Controllers/Front/Colleges/MajorController.php
+++ app/Http/Controllers/Front/Colleges/MajorController.php
@@ -153,7 +153,10 @@
 //                $majors[$i]->major_confirm_id = $major_confirms[$majors[$i]->major_confirm_id];
 //                $majors[$i]->major_follow_id = $major_follows[$majors[$i]->major_follow_id];
              
-                $majors[$i]->magor_logo_name = splicingImgStr('admin', 'info', $majors[$i]->magor_logo_name);
+                if(!empty($majors[$i]->magor_logo_name))
+                    $majors[$i]->magor_logo_name = splicingImgStr('admin', 'info', $majors[$i]->magor_logo_name);
+                if(!empty($majors[$i]->major_cover_name))
+                    $majors[$i]->major_cover_name = splicingImgStr('admin', 'info', $majors[$i]->major_cover_name);
                 if($majors[$i]->province !== '')
                     $majors[$i]->province = getProCity($majors[$i]->province);
                 $major_confirms_str = strChangeArr($majors[$i]->major_confirm,EXPLODE_STR);
