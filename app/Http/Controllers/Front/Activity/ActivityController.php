3a1c1bf026f66945a919473069305e07e5d47c65
lishnalei <2686685661@qq.com>
2019-03-04 22:00:02 +0800

    test

--- app/Http/Controllers/Front/Activity/ActivityController.php
+++ app/Http/Controllers/Front/Activity/ActivityController.php
@@ -117,8 +117,8 @@
                     foreach ($get_activitys['info'] as $key => $item) {
                         $now_time = time();
                         $get_activitys['info'][$key]->start_state = $now_time < $item->begin_time ? 0 : $now_time > $item->end_time ? 2 : 1;
-                        $get_activitys['info'][$key]->begin_time = date("m-d", $item->begin_time);
-                        $get_activitys['info'][$key]->end_time = date("m-d", $item->end_time);
+                        $get_activitys['info'][$key]->begin_time = date("m月d日", $item->begin_time);
+                        $get_activitys['info'][$key]->end_time = date("m月d日", $item->end_time);
                         if ($item->active_img != '')
                             $get_activitys['info'][$key]->active_img = splicingImgStr('admin', 'info', $item->active_img);
                         if ($item->magor_logo_name != '')
